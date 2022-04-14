b6fba3a1c60b541c8aa8b6999f6c7fd9c31757de
Marek Baczewski <marekbacz0808@gmail.com>
2022-04-14 21:05:16 +0200

    Client Websockets

--- src/requests/requests.jsx
+++
@@ -1,8 +0,0 @@
-const port = "8080";
-const baseURL = `http://localhost:${port}`;
-
-export const endpoints = {
-  createGame: baseURL + "/create",
-  connectToRandom: baseURL + "/connect/random",
-  connectToGame: baseURL + "/connect",
-};
